c2f085b0f31405a5b7d88796d532b423a8c2c62b
Steven You <yousilin@gmail.com>
2012-08-21 03:31:00 -0400

    name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='Muua Baby',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
+      author='Steven You',
       author_email='steven@gosteven.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask>=0.7.2'],
